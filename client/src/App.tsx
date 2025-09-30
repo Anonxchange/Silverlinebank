beb6ddb829e00862dc1dff57f8d3179f13bb5470
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-09-30 17:25:45 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import HomePage from "@/pages/HomePage";
+import SignOnPage from "@/pages/SignOnPage";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={HomePage} />
+      <Route path="/sign-on" component={SignOnPage} />
       <Route component={NotFound} />
     </Switch>
   );
